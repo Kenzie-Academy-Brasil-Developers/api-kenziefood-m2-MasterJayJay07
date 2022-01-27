2d85593a8197c37400891a7049c5745790b133a3
Thiago Henrique Monserrat Rezende <thiagohenriquemonserrat@gmail.com>
2022-01-26 22:33:35 -0300

    Estilo dos botões de busca

--- src/js/feature_vitrine/criar_vitrine.js
+++ src/js/feature_vitrine/criar_vitrine.js
@@ -1,5 +1,6 @@
 // recebendo objetos da API
 import {arrProdutos} from "../fetch_api/fetch_API.js";
+
 const listaDestaque = document.querySelector(".ulPratos")
 
 
@@ -12,17 +13,22 @@ const construirLayoutPratos = async (ulContainer, produtos, TipoBotao) => {
         //Criando as Tags para os cards de produtos
         let li = document.createElement("li")
         let img = document.createElement("img")
+        let icon = document.createElement("img")
         let h4 = document.createElement("h4")
         let h3 = document.createElement("h3")
         let p = document.createElement("p")
         let div = document.createElement("div")
         let span = document.createElement("span")
         let button = document.createElement("button")
+        let figure = document.createElement("figure")
 
         //Implementando os dados nas tags do card
         img.src = imagem
         img.alt = nome
         h4.innerText = categoria
+        // if(categoria==="Panificadora"){
+        //     document.h4.style.backgroundImage = 'url("pao.png")'
+        // }
         h3.innerText = nome
         p.innerText = descricao
         li.id = id
@@ -32,6 +38,8 @@ const construirLayoutPratos = async (ulContainer, produtos, TipoBotao) => {
 
         //Referenciando e montando layout    
         li.appendChild(img)
+        figure.appendChild(img)
+        li.appendChild(figure)
         li.appendChild(h4)
         li.appendChild(h3)
         li.appendChild(p)
@@ -44,10 +52,12 @@ const construirLayoutPratos = async (ulContainer, produtos, TipoBotao) => {
         //Adicionando Class na Li de produtos   
         li.classList.add("classePrato")
         button.classList.add(TipoBotao)
+        h4.classList.add("categoria")
     }
 
 }
 construirLayoutPratos(listaDestaque, arrProdutos, "adicionar")
+
 
 export {listaDestaque}
 
